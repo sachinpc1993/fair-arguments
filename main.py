26cad8950451155c47da7afb207db524b72027ce
Sachin PC <Sachinpc.1993@live.com>
2021-11-30 15:57:38 +1100

    Updated Main to exclude a topic
    Topic 25 needed to be excluded.

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ if __name__ == '__main__':
         system_temp_df = system_ranking[system_ranking['system_name'] == system]
 
         for i in range(1, 51):
-            ranking_temp_df = system_temp_df[system_temp_df['topic'] == i]
-            unfairness_rnd = rND.rND_fairness_ranking_calculation(ranking_temp_df)
-            unfairness_rrd = rRD.rRD_fairness_ranking_calculation(ranking_temp_df)
-            unfairness_rkl = rKL.rKL_fairness_ranking_calculation(ranking_temp_df)
-            unfairness_df.append([system, i, unfairness_rnd, unfairness_rkl, unfairness_rrd])
+            if i != 25:
+                ranking_temp_df = system_temp_df[system_temp_df['topic'] == i]
+                unfairness_rnd = rND.rND_fairness_ranking_calculation(ranking_temp_df)
+                unfairness_rrd = rRD.rRD_fairness_ranking_calculation(ranking_temp_df)
+                unfairness_rkl = rKL.rKL_fairness_ranking_calculation(ranking_temp_df)
+                unfairness_df.append([system, i, unfairness_rnd, unfairness_rkl, unfairness_rrd])
 
     unfairness = pd.DataFrame(unfairness_df,
                               columns=['system', 'topic', 'rND_unfairness', 'rKL_unfairness', 'rRD_unfairness'])
